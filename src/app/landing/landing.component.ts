5b614a4c8375811eabfa9d93ab2810ace34a1fd4
gordonbannier <gordon.bannier@emineo.ch>
2024-04-22 16:38:08 +0200

    styling

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -5,6 +5,9 @@ import {MeditainmentComponent} from "./meditainment/meditainment.component";
 import {RoutingUrlType} from "../models/routing.url.type.model";
 import {AppRoute} from "../models/app-route";
 import {CommonModule} from "@angular/common";
+import {MatIconModule, MatIconRegistry} from "@angular/material/icon";
+import {DomSanitizer} from "@angular/platform-browser";
+import {HttpClientModule} from "@angular/common/http";
 
 
 
@@ -19,8 +22,15 @@ import {CommonModule} from "@angular/common";
     RouterOutlet,
     RouterLink,
     RouterLinkActive,
+    MatIconModule,
+    HttpClientModule
   ],
   styleUrl: './landing.component.css'
 })
 export class LandingComponent {
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'thumbs-up',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/img/examples/thumbup-icon.svg'));
+  }
 }
